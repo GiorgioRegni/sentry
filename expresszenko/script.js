7c1f5a8429044c8256af105e2b8fd052cf6372fc
Tommy BISI <tbisi@e1z3r5p22.42.us.org>
2017-08-16 17:48:04 -0700

    new server with express js

---
+++ expresszenko/script.js
@@ -0,0 +1,444 @@
+
+"use strict";
+
+let data1 =
+	{	"timeRange":[1501570800000,1504249199999],
+		"storageUtilized":[0,6],
+		"incomingBytes":6,
+		"outgoingBytes":0,
+		"numberOfObjects":[0,1],
+		"operations":
+		{
+			"s3:DeleteBucket":0,
+			"s3:DeleteBucketCors":0,
+			"s3:DeleteBucketWebsite":0,
+			"s3:DeleteObjectTagging":0,
+			"s3:ListBucket":0,
+			"s3:GetBucketAcl":0,
+			"s3:GetBucketCors":0,
+			"s3:GetBucketWebsite":0,
+			"s3:GetBucketLocation":0,
+			"s3:CreateBucket":1,
+			"s3:PutBucketAcl":0,
+			"s3:PutBucketCors":0,
+			"s3:PutBucketWebsite":0,
+			"s3:PutObject":1,
+			"s3:CopyObject":0,
+			"s3:UploadPart":0,
+			"s3:ListBucketMultipartUploads":0,
+			"s3:ListMultipartUploadParts":0,
+			"s3:InitiateMultipartUpload":0,
+			"s3:CompleteMultipartUpload":0,
+			"s3:AbortMultipartUpload":0,
+			"s3:DeleteObject":0,
+			"s3:MultiObjectDelete":0,
+			"s3:GetObject":0,
+			"s3:GetObjectAcl":0,
+			"s3:GetObjectTagging":0,
+			"s3:PutObjectAcl":0,
+			"s3:PutObjectTagging":0,
+			"s3:HeadBucket":0,
+			"s3:HeadObject":0,
+			"s3:PutBucketVersioning":0,
+			"s3:GetBucketVersioning":0,
+			"s3:PutBucketReplication":0,
+			"s3:GetBucketReplication":0,
+			"s3:DeleteBucketReplication":0
+		},
+		"bucketName":"utapi-bucket"
+	};
+
+let data2 =
+	{	"timeRange":[1501570800000,1504249199999],
+		"storageUtilized":[0,6],
+		"incomingBytes":12,
+		"outgoingBytes":0,
+		"numberOfObjects":[0,2],
+		"operations":
+		{
+			"s3:DeleteBucket":0,
+			"s3:DeleteBucketCors":0,
+			"s3:DeleteBucketWebsite":0,
+			"s3:DeleteObjectTagging":0,
+			"s3:ListBucket":0,
+			"s3:GetBucketAcl":0,
+			"s3:GetBucketCors":0,
+			"s3:GetBucketWebsite":0,
+			"s3:GetBucketLocation":0,
+			"s3:CreateBucket":1,
+			"s3:PutBucketAcl":0,
+			"s3:PutBucketCors":0,
+			"s3:PutBucketWebsite":0,
+			"s3:PutObject":2,
+			"s3:CopyObject":0,
+			"s3:UploadPart":0,
+			"s3:ListBucketMultipartUploads":0,
+			"s3:ListMultipartUploadParts":0,
+			"s3:InitiateMultipartUpload":0,
+			"s3:CompleteMultipartUpload":0,
+			"s3:AbortMultipartUpload":0,
+			"s3:DeleteObject":0,
+			"s3:MultiObjectDelete":0,
+			"s3:GetObject":0,
+			"s3:GetObjectAcl":0,
+			"s3:GetObjectTagging":0,
+			"s3:PutObjectAcl":0,
+			"s3:PutObjectTagging":0,
+			"s3:HeadBucket":0,
+			"s3:HeadObject":0,
+			"s3:PutBucketVersioning":0,
+			"s3:GetBucketVersioning":0,
+			"s3:PutBucketReplication":0,
+			"s3:GetBucketReplication":0,
+			"s3:DeleteBucketReplication":0
+		},
+		"bucketName":"utapi-bucket"
+	};
+
+let data3 =
+	{	"timeRange":[1501570800000,1504249199999],
+		"storageUtilized":[0,6],
+		"incomingBytes":18,
+		"outgoingBytes":0,
+		"numberOfObjects":[0,3],
+		"operations":
+		{
+			"s3:DeleteBucket":0,
+			"s3:DeleteBucketCors":0,
+			"s3:DeleteBucketWebsite":0,
+			"s3:DeleteObjectTagging":0,
+			"s3:ListBucket":0,
+			"s3:GetBucketAcl":0,
+			"s3:GetBucketCors":0,
+			"s3:GetBucketWebsite":0,
+			"s3:GetBucketLocation":0,
+			"s3:CreateBucket":1,
+			"s3:PutBucketAcl":0,
+			"s3:PutBucketCors":0,
+			"s3:PutBucketWebsite":0,
+			"s3:PutObject":3,
+			"s3:CopyObject":0,
+			"s3:UploadPart":0,
+			"s3:ListBucketMultipartUploads":0,
+			"s3:ListMultipartUploadParts":0,
+			"s3:InitiateMultipartUpload":0,
+			"s3:CompleteMultipartUpload":0,
+			"s3:AbortMultipartUpload":0,
+			"s3:DeleteObject":0,
+			"s3:MultiObjectDelete":0,
+			"s3:GetObject":0,
+			"s3:GetObjectAcl":0,
+			"s3:GetObjectTagging":0,
+			"s3:PutObjectAcl":0,
+			"s3:PutObjectTagging":0,
+			"s3:HeadBucket":0,
+			"s3:HeadObject":0,
+			"s3:PutBucketVersioning":0,
+			"s3:GetBucketVersioning":0,
+			"s3:PutBucketReplication":0,
+			"s3:GetBucketReplication":0,
+			"s3:DeleteBucketReplication":0
+		},
+		"bucketName":"utapi-bucket"
+	};
+
+let data4 =
+	{	"timeRange":[1501570800000,1504249199999],
+		"storageUtilized":[0,6],
+		"incomingBytes":12,
+		"outgoingBytes":0,
+		"numberOfObjects":[0,3],
+		"operations":
+		{
+			"s3:DeleteBucket":0,
+			"s3:DeleteBucketCors":0,
+			"s3:DeleteBucketWebsite":0,
+			"s3:DeleteObjectTagging":0,
+			"s3:ListBucket":0,
+			"s3:GetBucketAcl":0,
+			"s3:GetBucketCors":0,
+			"s3:GetBucketWebsite":0,
+			"s3:GetBucketLocation":0,
+			"s3:CreateBucket":1,
+			"s3:PutBucketAcl":0,
+			"s3:PutBucketCors":0,
+			"s3:PutBucketWebsite":0,
+			"s3:PutObject":3,
+			"s3:CopyObject":0,
+			"s3:UploadPart":0,
+			"s3:ListBucketMultipartUploads":0,
+			"s3:ListMultipartUploadParts":0,
+			"s3:InitiateMultipartUpload":0,
+			"s3:CompleteMultipartUpload":0,
+			"s3:AbortMultipartUpload":0,
+			"s3:DeleteObject":0,
+			"s3:MultiObjectDelete":0,
+			"s3:GetObject":0,
+			"s3:GetObjectAcl":0,
+			"s3:GetObjectTagging":0,
+			"s3:PutObjectAcl":0,
+			"s3:PutObjectTagging":0,
+			"s3:HeadBucket":0,
+			"s3:HeadObject":0,
+			"s3:PutBucketVersioning":0,
+			"s3:GetBucketVersioning":0,
+			"s3:PutBucketReplication":0,
+			"s3:GetBucketReplication":0,
+			"s3:DeleteBucketReplication":0
+		},
+		"bucketName":"utapi-bucket"
+	};
+
+let data5 =
+	{	"timeRange":[1501570800000,1504249199999],
+		"storageUtilized":[0,6],
+		"incomingBytes":24,
+		"outgoingBytes":0,
+		"numberOfObjects":[0,3],
+		"operations":
+		{
+			"s3:DeleteBucket":0,
+			"s3:DeleteBucketCors":0,
+			"s3:DeleteBucketWebsite":0,
+			"s3:DeleteObjectTagging":0,
+			"s3:ListBucket":0,
+			"s3:GetBucketAcl":0,
+			"s3:GetBucketCors":0,
+			"s3:GetBucketWebsite":0,
+			"s3:GetBucketLocation":0,
+			"s3:CreateBucket":1,
+			"s3:PutBucketAcl":0,
+			"s3:PutBucketCors":0,
+			"s3:PutBucketWebsite":0,
+			"s3:PutObject":3,
+			"s3:CopyObject":0,
+			"s3:UploadPart":0,
+			"s3:ListBucketMultipartUploads":0,
+			"s3:ListMultipartUploadParts":0,
+			"s3:InitiateMultipartUpload":0,
+			"s3:CompleteMultipartUpload":0,
+			"s3:AbortMultipartUpload":0,
+			"s3:DeleteObject":0,
+			"s3:MultiObjectDelete":0,
+			"s3:GetObject":0,
+			"s3:GetObjectAcl":0,
+			"s3:GetObjectTagging":0,
+			"s3:PutObjectAcl":0,
+			"s3:PutObjectTagging":0,
+			"s3:HeadBucket":0,
+			"s3:HeadObject":0,
+			"s3:PutBucketVersioning":0,
+			"s3:GetBucketVersioning":0,
+			"s3:PutBucketReplication":0,
+			"s3:GetBucketReplication":0,
+			"s3:DeleteBucketReplication":0
+		},
+		"bucketName":"utapi-bucket"
+	};
+let data6 =
+	{	"timeRange":[1501570800000,1504249199999],
+		"storageUtilized":[0,6],
+		"incomingBytes":0,
+		"outgoingBytes":0,
+		"numberOfObjects":[0,3],
+		"operations":
+		{
+			"s3:DeleteBucket":0,
+			"s3:DeleteBucketCors":0,
+			"s3:DeleteBucketWebsite":0,
+			"s3:DeleteObjectTagging":0,
+			"s3:ListBucket":0,
+			"s3:GetBucketAcl":0,
+			"s3:GetBucketCors":0,
+			"s3:GetBucketWebsite":0,
+			"s3:GetBucketLocation":0,
+			"s3:CreateBucket":1,
+			"s3:PutBucketAcl":0,
+			"s3:PutBucketCors":0,
+			"s3:PutBucketWebsite":0,
+			"s3:PutObject":3,
+			"s3:CopyObject":0,
+			"s3:UploadPart":0,
+			"s3:ListBucketMultipartUploads":0,
+			"s3:ListMultipartUploadParts":0,
+			"s3:InitiateMultipartUpload":0,
+			"s3:CompleteMultipartUpload":0,
+			"s3:AbortMultipartUpload":0,
+			"s3:DeleteObject":0,
+			"s3:MultiObjectDelete":0,
+			"s3:GetObject":0,
+			"s3:GetObjectAcl":0,
+			"s3:GetObjectTagging":0,
+			"s3:PutObjectAcl":0,
+			"s3:PutObjectTagging":0,
+			"s3:HeadBucket":0,
+			"s3:HeadObject":0,
+			"s3:PutBucketVersioning":0,
+			"s3:GetBucketVersioning":0,
+			"s3:PutBucketReplication":0,
+			"s3:GetBucketReplication":0,
+			"s3:DeleteBucketReplication":0
+		},
+		"bucketName":"utapi-bucket"
+	};
+
+let data7 =
+	{	"timeRange":[1501570800000,1504249199999],
+		"storageUtilized":[0,6],
+		"incomingBytes":6,
+		"outgoingBytes":0,
+		"numberOfObjects":[0,3],
+		"operations":
+		{
+			"s3:DeleteBucket":0,
+			"s3:DeleteBucketCors":0,
+			"s3:DeleteBucketWebsite":0,
+			"s3:DeleteObjectTagging":0,
+			"s3:ListBucket":0,
+			"s3:GetBucketAcl":0,
+			"s3:GetBucketCors":0,
+			"s3:GetBucketWebsite":0,
+			"s3:GetBucketLocation":0,
+			"s3:CreateBucket":1,
+			"s3:PutBucketAcl":0,
+			"s3:PutBucketCors":0,
+			"s3:PutBucketWebsite":0,
+			"s3:PutObject":3,
+			"s3:CopyObject":0,
+			"s3:UploadPart":0,
+			"s3:ListBucketMultipartUploads":0,
+			"s3:ListMultipartUploadParts":0,
+			"s3:InitiateMultipartUpload":0,
+			"s3:CompleteMultipartUpload":0,
+			"s3:AbortMultipartUpload":0,
+			"s3:DeleteObject":0,
+			"s3:MultiObjectDelete":0,
+			"s3:GetObject":0,
+			"s3:GetObjectAcl":0,
+			"s3:GetObjectTagging":0,
+			"s3:PutObjectAcl":0,
+			"s3:PutObjectTagging":0,
+			"s3:HeadBucket":0,
+			"s3:HeadObject":0,
+			"s3:PutBucketVersioning":0,
+			"s3:GetBucketVersioning":0,
+			"s3:PutBucketReplication":0,
+			"s3:GetBucketReplication":0,
+			"s3:DeleteBucketReplication":0
+		},
+		"bucketName":"utapi-bucket"
+	};
+
+let arr = [data1, data2, data3, data4, data5, data6, data7, data2, data3, data1, data5, data1, data2, data7, data6, data5, data2, data3, data1, data5, data1, data2, data7, data6, data5,data2, data3, data1, data5, data1, data2, data7, data6, data5];
+
+function buildChart(arr) {
+
+	let	width = 500,
+		height = 300,
+		animateDuration = 700,
+		animateDelay = 30;
+
+	let incomingBytesArr = arr.map(function(i) { return i.incomingBytes; });
+	let dates = arr.map(function(i) { return i.timeRange[0]; })
+	// console.log(incomingBytesArr);
+
+	var tooltip = d3.select("body").append('div')
+					.style("position", "absolute")
+					.style("background", "#f4f4f4")
+					.style("padding", "5 15px")
+					.style("border", "1px #333 solid")
+					.style("border-radius", "5 px")
+					.style("opacity", "0")
+
+	let date_format = d3.time.format("%b.%d.%Y %H:%M");
+
+	let y = d3.scale.linear()
+					.domain([0, d3.max(incomingBytesArr)])
+					.range([12, height - 12]);
+
+	let x = d3.scale.ordinal()
+					.domain(d3.range(0, incomingBytesArr.length))
+					.rangeBands([0, width]);
+
+	let colors = d3.scale.linear()
+					.domain([0, incomingBytesArr.length])
+					.range(["#90ee90", "#30c230"]);
+
+	let canvas = d3.select("#chart").append("svg")
+					.attr("width", width)
+					.attr("height", height)
+					.style("background", "#f4f4f4");
+
+	let bars = 	canvas.selectAll("rect")
+					.data(incomingBytesArr)
+					.enter()
+					.append("rect")
+					.attr("x", function(d, i) { return x(i); })
+					.attr("y", height)
+					.attr("width", x.rangeBand())
+					.attr("height",0)
+					.style("fill", function(d, i) { return colors(d); })
+					.on("mouseover", function(d) {
+						tooltip.transition()
+					 		.style("opacity", 1)
+
+						tooltip.html(d)
+							.style("left", (d3.event.pageX) + "px")
+							.style("top", (d3.event.pageY + "px"))
+						d3.select(this).style("opacity", 0.5)})
+					.on("mouseout", function(d) {
+						tooltip.transition()
+							.style("opacity", 0)
+						d3.select(this).style("opacity", 1)});
+
+	bars.transition()
+					.attr("height", function(d) { return y(d); })
+					.attr("y", function(d) { return height - y(d); })
+					.duration(animateDuration)
+					.delay(function(d, i) { return i * animateDelay; })
+					.ease("elastic")
+
+	let vScale = d3.scale.linear()
+					.domain([0, d3.max(incomingBytesArr)])
+					.range([height, 0]);
+
+	let hScale = d3.scale.ordinal()
+					.domain(d3.range(0, incomingBytesArr.length))
+					.rangeBands([0, width]);
+
+	// v Axis
+	let vAxis = d3.svg.axis()
+					.scale(vScale)
+					.orient("left")
+					.ticks(5)
+					.tickPadding(6);
+
+	// v Guide
+	let vGuide = d3.select("svg")
+					.append("g")
+						vAxis(vGuide)
+						vGuide.attr("transform", "translate(35, 10)")
+						vGuide.selectAll("path")
+						.style("fill", "none")
+						.style("stroke", "#000")
+						vGuide.selectAll("line")
+						.style("stroke", "#000")
+
+	let hAxis = d3.svg.axis()
+					.scale(vScale)
+					.orient("bottom")
+					.tickValues(hScale.domain().filter(function(d, i) { return !(i % (incomingBytesArr.length / 5)) }))
+	// v Guide
+	let hGuide = d3.select("svg")
+					.append("g")
+						hAxis(hGuide)
+						hGuide.attr("transform", "translate(35, 10)")
+						hGuide.selectAll("path")
+						.style("fill", "none")
+						.style("stroke", "#000")
+						hGuide.selectAll("line")
+						.style("stroke", "#000")
+
+}
+
+buildChart(arr);
