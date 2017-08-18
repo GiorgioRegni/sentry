340f3f9f9965da68706a4855a3a906b54bc2e541
Tomás Bisi <tomasbisi@MacBook-Pro.local>
2017-08-18 10:11:41 -0700

    pushing last version with last backened changes

---
+++ script.js
@@ -0,0 +1,225 @@
+/**
+ * @Author: Anhelina Shulha <anhelinashulha>
+ * @Date:   Aug-14-2017
+ * @Email:  anhelina.shulha@gmail.com
+ * @Filename: script.js
+ * @Last modified by:   anhelinashulha
+ * @Last modified time: Aug-17-2017
+ */
+
+var arr = [];
+
+$(document).ready(function() {
+	$.getJSON('http://localhost:8080/user').done(function(data) {
+		fixdata(data);
+	})
+});
+
+function fixdata(data) {
+	var objArray = JSON.parse(data);
+	for (var i = 0; i < objArray.length; i++)
+	{
+		arr.push((JSON.parse(objArray[i]))[0]);
+	}
+	buildChart(arr);
+}
+
+function incBytesChart(incomingBytesArr, dates) {
+	const barChart = document.getElementById('incBytesChart');
+	let chart = new Chart(barChart, {
+	type: 'bar',
+
+	data: {
+		labels: dates,
+		datasets: [{
+			label: 'bytes',
+			data: incomingBytesArr,
+			backgroundColor: 'hsla(44, 100%, 60%, 0.70)',
+			borderColor: 'hsla(44, 100%, 40%, 1)',
+			borderWidth: 2 }]},
+
+	options: {
+		responsive: true,
+		title:{
+			display: true,
+			text:'incoming bytes',
+		 	fontColor: "#424242" },
+		legend: {
+			display: false },
+		scales: {
+			yAxes: [{
+				ticks: {
+					fontColor: "#f",
+					fontSize: 11,
+					beginAtZero: true,
+					userCallback: function(value, index, values) {
+						if (value >= 1000000000000) {
+							return value / 1000000000000 + 'T';
+						} else if (value >= 1000000000) {
+							return value / 1000000000 + 'G';
+						} else if (value >= 1000000) {
+							return value / 1000000 + 'M';
+						} else if (value >= 1000) {
+							return value / 1000 + 'k';
+						} else if (value < 10) {
+							return value.toPrecision(2);
+						}
+						return value; }},
+				gridLines: {
+					color: 'hsla(0, 0%, 75%, 0.84)',
+					lineWidth: 0.8 }}],
+			xAxes: [{
+				display: false,
+				gridLines: {
+					color: 'hsla(0, 0%, 75%, 0.84)',
+					lineWidth: 0.8 }}]}}});
+}
+
+function storUtilChart(storageUtilizedArr, dates) {
+	const ctx = document.getElementById('storageUtilChart');
+	let chart = new Chart(ctx, {
+	type: 'line',
+
+	data: {
+		labels: dates,
+		datasets: [{
+			label: 'storage utilized (bytes)',
+			data: storageUtilizedArr,
+			backgroundColor: 'hsla(177, 100%, 35%, 0.70)',
+			borderColor: 'hsla(177, 100%, 25%, 1)'
+			}]},
+
+	options: {
+		responsive: true,
+		stacked: false,
+		title:{
+			display: true,
+			text:'storage utilized',
+		 	fontColor: "#424242" },
+		legend: {
+			display: false },
+		scales: {
+			yAxes: [{
+				type: "linear",
+					display: true,
+					position: "left",
+					ticks: {
+						fontColor: "#424242",
+						fontSize: 11,
+						beginAtZero: true,
+						userCallback: function(value, index, values) {
+							if (value >= 1000000000000) {
+								return value / 1000000000000 + 'T';
+							} else if (value >= 1000000000) {
+								return value / 1000000000 + 'G';
+							} else if (value >= 1000000) {
+								return value / 1000000 + 'M';
+							} else if (value >= 1000) {
+								return value / 1000 + 'k';
+							} else if (value < 10) {
+								return value.toPrecision(2);
+							}
+							return value; }},
+					gridLines: {
+						color: 'hsla(0, 0%, 75%, 0.84)',
+						lineWidth: 0.8
+				}}],
+			xAxes: [{
+				display: false,
+				gridLines: {
+					color: 'hsla(0, 0%, 75%, 0.84)',
+					lineWidth: 0.8 }}]}}});
+}
+
+function numObjectsChart(numberOfObjectsArr, dates) {
+	const ctx = document.getElementById('numOfObjectsChart');
+	let chart = new Chart(ctx, {
+		type: 'line',
+
+		data: {
+			labels: dates,
+			datasets: [{
+				label: 'number of objects',
+				data: numberOfObjectsArr,
+				backgroundColor: "hsla(328, 81%, 51%, 0.70)",
+				borderColor: 'hsla(328, 81%, 41%, 1)' }]},
+
+		options: {
+			responsive: true,
+			stacked: false,
+			title:{
+				display: true,
+				text:'number of objects',
+				fontColor: "#424242" },
+			legend: {
+				display: false },
+			scales: {
+				yAxes: [{
+					type: "linear",
+					display: true,
+					position: "left",
+					ticks: {
+						fontColor: "#424242",
+						fontSize: 11,
+						beginAtZero: true,
+						userCallback: function(value, index, values) {
+							if (value >= 1000000000000) {
+								return value / 1000000000000 + 'T';
+							} else if (value >= 1000000000) {
+								return value / 1000000000 + 'G';
+							} else if (value >= 1000000) {
+								return value / 1000000 + 'M';
+							} else if (value >= 1000) {
+								return value / 1000 + 'k';
+							} else if (value < 10) {
+								return value.toPrecision(2);
+							}
+							return value; }},
+						gridLines: {
+							color: 'hsla(0, 0%, 75%, 0.84)',
+							lineWidth: 0.8
+					}}],
+					xAxes: [{
+						display: false,
+						gridLines: {
+							color: 'hsla(0, 0%, 75%, 0.84)',
+							lineWidth: 0.8 }}]}}});
+
+}
+
+function buildChart(arr) {
+
+	let incomingBytesArr = arr.map(function(i) { return i.incomingBytes; });
+
+	let storageUtilizedArr = arr.map(function(i) { return i.storageUtilized[1]; });
+
+	let numberOfObjectsArr = arr.map(function(i) { return i.numberOfObjects[1]; });
+
+	let dates = arr.map(function(i) {
+		let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+			"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+		let date = new Date(i.timeRange[0]);
+		let day = date.getDate();
+			if (day < 10) day = "0" + day;
+		let hours = date.getHours();
+			if (hours < 10) hours = "0" + hours;
+		let minutes = date.getMinutes();
+			if (minutes < 10) minutes = "0" + minutes;
+
+		let dateStart = monthNames[date.getMonth()] + " " + day + " " + hours + ":" + minutes;
+
+		date = new Date(i.timeRange[1]);
+		day = date.getDate();
+		if (day < 10) day = "0" + day;
+		hours = date.getHours();
+		if (hours < 10) hours = "0" + hours;
+		minutes = date.getMinutes();
+		if (minutes < 10) minutes = "0" + minutes;
+		let dateEnd = monthNames[date.getMonth()] + " " + day + " " + hours + ":" + minutes;
+
+		return dateStart + " | " + dateEnd; })
+
+	incBytesChart(incomingBytesArr, dates);
+	storUtilChart(storageUtilizedArr, dates);
+	numObjectsChart(numberOfObjectsArr, dates);
+}
---
+++ server.js
@@ -0,0 +1,214 @@
+var express = require('express');
+var path = require('path');
+var aws4 = require('aws4');
+var http = require('http');
+var formidable = require("formidable");
+var app = express();
+const jsdom = require("jsdom");
+var totalData;
+
+app.get('/script.js',function(req,res){
+    res.sendFile(path.join(__dirname + '/script.js'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/', function(req, res) {
+    res.redirect('/index.html');
+});
+
+app.listen(8080);
+console.log("Listening on Port 8080");
+
+app.get('/user', function(req, res, next) {
+  res.json(totalData);
+});
+
+app.post('/', function(req, res, next) {
+	var	form = new formidable.IncomingForm();
+	fields = {};
+
+	form
+	  .on('error', function(err) {
+		res.writeHead(500, {'content-type': 'text/plain'});
+		res.end('error:\n\n'+util.inspect(err));
+		console.error(err);
+	  })
+	  .on('field', function(field, value) {
+		console.log(field, value);
+		fields[field] = value;
+	  })
+	  .on('end', function() {
+		console.log(fields);
+		dateconvert(fields, (data) => {
+			res.sendFile(path.join(__dirname + '/file.html'));
+		});
+	  });
+	form.parse(req);
+});
+
+app.get('/img/zenko-logo.png',function(req,res){
+    res.sendFile(path.join(__dirname + '/img/zenko-logo.png'));
+});
+
+app.get('/css/jquery.fullPage.css',function(req,res){
+    res.sendFile(path.join(__dirname + '/css/jquery.fullPage.css'));
+});
+
+app.get('/css/style.css',function(req,res){
+    res.sendFile(path.join(__dirname + '/css/style.css'));
+});
+
+app.get('/js/jquery.fullPage.min.js',function(req,res){
+    res.sendFile(path.join(__dirname + '/js/jquery.fullPage.min.js'));
+});
+
+var objArray = [];
+function callServiceApi(name, accessKeyId, secretAccessKey, startTime, endTime, cb) {
+	console.log("Service Api called");
+	const token = '';
+	const requestBody = JSON.stringify({
+		service: ['s3'],
+		timeRange: [startTime, endTime],
+	});
+	const header = {
+		host: 'localhost',
+		port: 8100,
+		method: 'POST',
+		service: 's3',
+		path: '/service?Action=ListMetrics',
+		signQuery: false,
+		body: requestBody,
+	};
+	const credentials = { accessKeyId, secretAccessKey, token };
+	const options = aws4.sign(header, credentials);
+	const request = http.request(options, response => {
+		const body = [];
+		response.on('data', chunk => body.push(chunk));
+		response.on('end', () => cb(null, `${body.join('')}`));
+	});
+	request.on('error', e => process.stdout.write(`error: ${e.message}\n`));
+	request.write(requestBody);
+	request.end();
+}
+
+function callBucketApi(name, accessKeyId, secretAccessKey, startTime, endTime, cb) {
+	console.log("Bucket Api called");
+	const token = '';
+	const requestBody = JSON.stringify({
+		buckets: [name],
+		timeRange: [startTime, endTime],
+	});
+	const header = {
+		host: 'localhost',
+		port: 8100,
+		method: 'POST',
+		service: 's3',
+		path: '/buckets?Action=ListMetrics',
+		signQuery: false,
+		body: requestBody,
+	};
+	const credentials = { accessKeyId, secretAccessKey, token };
+	const options = aws4.sign(header, credentials);
+	const request = http.request(options, response => {
+		const body = [];
+		response.on('data', chunk => body.push(chunk));
+		response.on('end', () => cb(null, `${body.join('')}`));
+	});
+	request.on('error', e => process.stdout.write(`error: ${e.message}\n`));
+	request.write(requestBody);
+	request.end();
+}
+
+function miliseconds(hrs,min)
+{
+    return((hrs*60*60+min*60)*1000);
+}
+
+function getRange(name, accessKeyId, secretAccessKey, option, Start, End, Interval, cb) {
+	var counter = 0;
+	var iterations;
+	var callLimit = 10;
+	var checkLimit = Math.floor((End - Start) / Interval);
+	if (checkLimit > callLimit)
+	{
+		Interval = Math.ceil((End - Start) / callLimit);
+	}
+	Interval = Math.ceil(Interval / 900000.0) * 900000;
+	var Next = Start + Interval - 1;
+	iterations = Math.ceil((End - Start) / Interval);
+
+	console.log("Name", name);
+	console.log("Interval", Interval);
+	console.log("Start", Start);
+	console.log("End", End);
+	console.log("Option", option);
+
+	if (option == 0)
+	{
+		for (var i = Start; i < End; i += Interval)
+		{
+			callServiceApi(name, accessKeyId, secretAccessKey, i, Next, function(err, result) {
+				objArray.push(result);
+				counter += 1;
+				if (counter == iterations) {
+					console.log("API Output", objArray.length)
+					cb(null, objArray);
+				} });
+				Next += Interval;
+			}
+	}
+	else
+	{
+		for (var i = Start; i < End; i += Interval)
+		{
+			callBucketApi(name, accessKeyId, secretAccessKey, i, Next, function(err, result) {
+				objArray.push(result);
+				counter += 1;
+				if (counter == iterations) {
+					console.log("API Output", objArray.length)
+					cb(null, objArray);
+				} });
+				Next += Interval;
+		}
+	}
+}
+
+function dateconvert(obj, cb){
+	const accessKeyId = obj.accesskey;
+	const secretAccessKey = obj.secretkey;
+	var option;
+	if (obj.type == 'Service Level')
+		option = 0;
+	else
+		option = 1;
+	var Start = new Date(obj.dateStart + 'T' + obj.timeStart).getTime();
+	var End = new Date(obj.dateEnd + 'T' + obj.timeEnd).getTime();
+	var Interval;
+	if (obj.timeStamp == '15 min')
+		Interval = miliseconds(0, 15);
+	else if (obj.timeStamp == '30 min')
+		Interval = miliseconds(0, 30);
+	else if (obj.timeStamp == '01 hr')
+		Interval = miliseconds(1, 0);
+	else if (obj.timeStamp == '06 hrs')
+		Interval = miliseconds(6, 0);
+	else if (obj.timeStamp == '12 hrs')
+		Interval = miliseconds(12, 0);
+	else if (obj.timeStamp == '01 day')
+		Interval = miliseconds(24, 0);
+	else if (obj.timeStamp == '15 days')
+		Interval = miliseconds(360, 0);
+	else if (obj.timeStamp == '01 month')
+		Interval = miliseconds(720, 15);
+	else
+		Interval = miliseconds(0, 30);
+	var name = obj.name;
+	getRange(name, accessKeyId, secretAccessKey, option, Start, End, Interval, () => {
+		console.log(objArray);
+		totalData = JSON.stringify(objArray);
+		cb(null, totalData);
+	});
+}
